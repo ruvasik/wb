ef75aaaeb3cdc7c89d3d0a652764fe69c5833e0d
Vladimir Alekseev <mail@vlal.ru>
2021-12-03 12:09:55 +0300

    BETA

--- wbphones.js
+++ wbphones.js
@@ -1,58 +1,60 @@
-// Checking page title
-if (document.title.indexOf("WBPoint") != -1) {
-  var s = document.createElement('script');
-  s.src = chrome.runtime.getURL('wbrequest.js');
-  s.onload = function() {
-    this.remove();
+(() => {
+  const getItem = (name, phone) => {
+    const data = JSON.parse(sessionStorage.getItem('wbdataxxxx') || '[]');
+
+    const _phone = phone.slice(-5).replace('-', '');
+
+    return data.find(item => {
+      const _name = item.buyer.name;
+      const mobile = item.buyer.mobile.toString();
+
+      return mobile.slice(-4) === _phone && name === _name;
+    })
   };
-  (document.head || document.documentElement).appendChild(s);
 
-  let listNode = null;
+  if (document.title.indexOf("WBPoint") != -1) {
+    const SSKEY = 'wbdataxxxx';
 
-  let observer = new MutationObserver(mutations => {
-    for(let mutation of mutations) {
-      for(let addedNode of mutation.addedNodes) {
-        if ((addedNode?.tagName || '').toLowerCase() === 'app-ready-item') {
-          const nodeData = {};
-          // console.log(addedNode);
-          nodeData.name = addedNode.querySelector('div.opp-item-data-entry span.opp-item-hoverlight').textContent;
-          nodeData.phone = addedNode.querySelector('div.opp-item__cell--separated div.opp-item-data-entry--s span').textContent;
+    var s = document.createElement('script');
+    s.src = chrome.runtime.getURL('wbrequest.js');
+    s.onload = function() {
+      this.remove();
+    };
+    (document.head || document.documentElement).appendChild(s);
 
-          console.log(nodeData);
+    let listNode = null;
+
+    let observer = new MutationObserver(mutations => {
+      for(let mutation of mutations) {
+        for(let addedNode of mutation.addedNodes) {
+          if ((addedNode?.tagName || '').toLowerCase() === 'app-ready-item') {
+            const nameNode = addedNode.querySelector('div.opp-item-data-entry span.opp-item-hoverlight');
+            const phoneNode = addedNode.querySelector('div.opp-item__cell--separated div.opp-item-data-entry--s span');
+
+            const user = getItem(nameNode.textContent, phoneNode.textContent);
+
+            if (user) {
+              phoneNode.innerHTML = user.buyer.mobile;
+            }
+          }
         }
       }
-    }
-  });
+    });
 
-  let safeCounter = 0;
-  const timer = setInterval(() => {
-    safeCounter++;
+    let safeCounter = 0;
+    const timer = setInterval(() => {
+      safeCounter++;
 
-    listNode = document.querySelector('.opp-manager--fw.opp-apps-list');
+      listNode = document.querySelector('.opp-manager--fw.opp-apps-list');
 
-    if (listNode) {
-      clearInterval(timer);
+      if (listNode) {
+        clearInterval(timer);
 
-      // setTimeout(() => {
-      //   fetch('https://s-point.wb.ru/s10/api/v2/pickpoint/16001/switch', {
-      //     headers: {
-      //       'x-token': localStorage.getItem('_wb-ext-user-token'),
-      //       'accept': 'application/json, text/plain, */*',
-      //       'accept-encoding': 'gzip, deflate, br',
-      //     },
-      //   })
-      //     .then((response) => {
-      //       return response.json();
-      //     })
-      //     .then((data) => {
-      //       console.log('DATA', data);
-      //     });
-      // }, 5000);
+        observer.observe(listNode, { childList: true, subtree: true });
+      }
 
-      observer.observe(listNode, { childList: true, subtree: true });
-    }
-
-    if (safeCounter > 100)
-      clearInterval(timer);
-  }, 200);
-}
+      if (safeCounter > 100)
+        clearInterval(timer);
+    }, 200);
+  }
+})()
--- wbrequest.js
+++ wbrequest.js
@@ -1,6 +1,18 @@
-console.log('WB#4');
+
 
 (function() {
+  const SSKEY = 'wbdataxxxx';
+
+  const uniq = (list) => {
+    const data = {};
+
+    list.forEach(item => {
+      data[`${item.id}-${item.rid}`] = item;
+    });
+
+    return Object.values(data);
+  }
+
   var proxied = window.XMLHttpRequest.prototype.open;
   window.XMLHttpRequest.prototype.open = function() {
     this.onreadystatechange = function (e) {
@@ -9,7 +21,13 @@ console.log('WB#4');
 
         if (url.indexOf('https://s-point.wb.ru/s10/api/v2/pickpoint/goods/list') > -1) {
           const resp = JSON.parse(this.responseText);
-          console.log('WB#8', this.responseURL, resp);
+
+          if (resp?.total > 0) {
+            let data = JSON.parse(sessionStorage.getItem(SSKEY) || '[]');
+            data = [...data, ...resp.data];
+
+            sessionStorage.setItem(SSKEY, JSON.stringify(uniq(data)));
+          }
         }
       }
     };
